d5866ae27cd7188c16e60e218b31e3001bfd888e
ffeliciawijayaa <feliciawijaya1910@gmail.com>
2026-07-14 17:07:40 +0700

    feat: jwt auth

---
+++ user-service/src/main/java/com/api/user/model/UserModel.java
@@ -0,0 +1,35 @@
+package com.api.user.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private String phone;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}
